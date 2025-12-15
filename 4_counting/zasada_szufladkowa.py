d45714c1ceb76f6a9929215180901f27ca9f7ff4
unknown <bz.bartosz.zukowski@gmail.com>
2025-12-15 16:51:11 +0100

    correction

--- 4_counting/zasada_szufladkowa.py
+++ 4_counting/zasada_szufladkowa.py
@@ -4,11 +4,12 @@ Znajdź najmniejszą dodatnią liczbę całkowitą (większą od 0), której nie
 
 
 def solution(A):
-  A_set = set(A)
-  for i in range(1, len(A) + 2):
-    if i not in A_set:
-      return i
-  return 1
+    A_set = set(A)
+    for i in range(1, len(A) + 2):
+        if i not in A_set:
+            return i
+    return 1
+
 
 print(solution(A))
 print(solution([1, 2, 3]))
